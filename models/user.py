447331d60bad7e73fc5840da0d54b32eab62931c
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-05-31 02:31:06 +0900

    feat: User API 및 JWT 인증 기능 구현
    swagger 문서화, jwt 토큰 구현, user api 구현

---
+++ models/user.py
@@ -0,0 +1,35 @@
+from pymongo import MongoClient
+from werkzeug.security import generate_password_hash, check_password_hash
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['albumate']
+
+class User:
+    def __init__(self):
+        self.collection = db['users']
+
+    def find_by_username(self, username):
+        return self.collection.find_one({'username': username})
+
+    def find_by_nickname(self, nickname):
+        return self.collection.find_one({'nickname': nickname})
+
+    def create_user(self, username, password, nickname):
+        if self.collection.find_one({'username': username}) or self.collection.find_one({'nickname': nickname}):
+            return None
+
+        hashed_password = generate_password_hash(password)
+        user_id = self.collection.insert_one({
+            'username': username,
+            'password': hashed_password,
+            'nickname': nickname
+        }).inserted_id
+        return str(user_id)
+
+    def verify_user(self, username, password):
+        user = self.find_by_username(username)
+        if not user:
+            return None
+        if not check_password_hash(user['password'], password):
+            return None
+        return user
